d5cb3a6962f8f064a10ca9a3da93e9e064b1735e
sakurakotubaki <gitonly543@gmail.com>
2026-02-03 07:33:55 +0900

    Initial Commit

---
+++ CommuRefactor/CommuRefactorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommuRefactorApp.swift
+//  CommuRefactor
+//
+//  Created by 橋本純一 on 2026/02/03.
+//
+
+import SwiftUI
+
+@main
+struct CommuRefactorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
